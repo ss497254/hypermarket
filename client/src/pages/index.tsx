311e9d731dd475ddb34b17974ec22832715f02ac
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-04-21 04:00:25 +0530

    update: index page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Logo } from "src/ui/Logo";
 
 const Home = () => {
   return (
-    <div className="h-screen c flex-col">
+    <div className="flex-col h-screen space-y-4 c">
+      <Logo size={180} />
       <h2>Welcome</h2>
-      <p className="text-lg text-emerald-500 font-semibold">
-        Still in progress...
+      <p className="max-w-lg mx-6 text-lg text-center text-gray-700">
+        Hypermarket, a supermarket automation system is a comprehensive solution
+        for streamlining the operations of a supermarket.
       </p>
     </div>
   );
