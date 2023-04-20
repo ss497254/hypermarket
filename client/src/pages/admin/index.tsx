1314fd007b1b98172e3ac8064915a7c5b05eae50
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-04-21 03:46:45 +0530

    feat: add admin update card on home

--- client/src/pages/admin/index.tsx
+++ client/src/pages/admin/index.tsx
@@ -1,12 +1,70 @@
+import { useForm } from "react-hook-form";
 import AdminLayout from "src/components/layouts/AdminLayout";
 import { useAdminStore } from "src/global-stores/useAdminStore";
+import { usePut } from "src/hooks/ApiHooks";
+import { showToast } from "src/lib/showToast";
+import { Button } from "src/ui/Button";
+import { Card } from "src/ui/Card";
+import { Input } from "src/ui/Input";
 
 const Home = () => {
   const { admin } = useAdminStore();
+  const { run, loading, error } = usePut("/api/admin/me");
+  const { register, handleSubmit, reset } = useForm({
+    values: { ...admin, password: "" },
+  });
 
   return (
-    <div className="relative flex items-center justify-center min-h-screen bg-indigo-300">
-      {admin?.username}
+    <div className="relative min-h-screen p-8 bg-indigo-300">
+      <Card
+        heading="Admin Details"
+        className="space-y-4"
+        footer={
+          <Button
+            className="!w-40"
+            loading={loading}
+            onClick={handleSubmit(
+              async (data: any) => {
+                const res = await run(data);
+                if (res && res.success) {
+                  reset();
+                } else {
+                }
+              },
+              () => {
+                showToast({ message: "Cannot add staff" }, "error");
+              },
+            )}
+          >
+            Save
+          </Button>
+        }
+      >
+        <Input
+          label="Username"
+          containerClassName="flex-grow"
+          required
+          {...register("username", { required: true })}
+        />
+        <Input
+          label="Firstname"
+          containerClassName="flex-grow"
+          required
+          {...register("firstName", { required: true })}
+        />
+        <Input
+          label="Lastname"
+          containerClassName="flex-grow"
+          required
+          {...register("lastName", { required: true })}
+        />
+        <Input
+          label="Password"
+          type="password"
+          required
+          {...register("password", { required: true })}
+        />
+      </Card>
     </div>
   );
 };
--- client/src/ui/Card.tsx
+++ client/src/ui/Card.tsx
@@ -4,16 +4,27 @@ interface props {
   footer: React.ReactNode;
   children: React.ReactNode;
   heading: string;
+  className?: string;
 }
 
-export const Card: React.FC<props> = ({ footer, heading, children }) => {
+export const Card: React.FC<props> = ({
+  footer,
+  heading,
+  children,
+  className,
+}) => {
   return (
-    <div className="border-500 mb-4 rounded-lg bg-dark-800 lg:mb-6">
-      <h4 className="p-5 text-2xl font-bold md:p-8">{heading}</h4>
-      <div className="border-y border-dark-600 p-5 text-base md:p-8">
+    <div className="mb-4 bg-white rounded-lg border-500 lg:mb-6">
+      <h4 className="p-4 text-2xl font-bold md:p-6">{heading}</h4>
+      <div
+        className={[
+          "p-5 text-base border-gray-300 border-y md:p-8",
+          className,
+        ].join(" ")}
+      >
         {children}
       </div>
-      <div className="flex justify-end rounded-b-lg bg-dark-700 p-4">
+      <div className="flex justify-end p-4 bg-gray-200 rounded-b-lg">
         {footer}
       </div>
     </div>
